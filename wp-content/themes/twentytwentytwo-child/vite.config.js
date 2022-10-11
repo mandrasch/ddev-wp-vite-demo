c45e7fa42d393aa59642b1de8efeff582cb15bda
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-10-11 21:45:23 +0200

    remove sample code

--- wp-content/themes/twentytwentytwo-child/vite.config.js
+++ wp-content/themes/twentytwentytwo-child/vite.config.js
@@ -4,12 +4,6 @@ import create_config from '@kucrut/vite-for-wp';
 
 export default create_config('js/src/app.js', 'js/dist', {
     plugins: [],
-    // https://getbootstrap.com/docs/5.2/getting-started/vite/
-    /*resolve: {
-      alias: {
-        "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
-      },
-    },*/
     server: {
         host: "0.0.0.0", // leave this unchanged for DDEV!
         port: 5173,
